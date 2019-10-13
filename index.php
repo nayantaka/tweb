476525a2c72150302fe721b91a3956a0caefa38a
nayantaka <nayantaka@gmail.com>
2019-10-13 10:57:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head><title>Contoh gabungan html dan php</title>
+<link rel="stylesheet" type="text/css" href="catering.css">
+<head>
+<body>
+
+	<div id="kontainer">
+		<div id="header">
+			<h1>Jasa Catering Indah</h1>
+		</div>
+		<div id="menu">
+			<h3>Menu</h3>
+			<ol>
+				<li>Home</li>
+				<li>Harga</li>
+				<li>Contact</li>
+		</div>
+		<div id="utama">
+		<h3>Selamat datang di catering Indah</h3>
+		<img src="tumpeng.jpeg" width=40%>
+		<p>
+			Berkah Catering adalah perusahaan Catering yang terus berkomitmen untuk memenuhi kebutuhan korporasi di Semarang atas santapan yang sehat, bernutrisi, berkualitas dan mempunyai sentuhan personal.
+
+			Kami menawarkan jasa catering untuk berbagai acara seperti ulang tahun, acara peresmian, pesta keluarga, pesta pernikahan dan dan acara-acara besar perusahaan.
+		</p>
+		</div>
+	</div>
+</body>
+</html>
